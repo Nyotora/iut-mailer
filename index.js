a731291ee6ea455011dc35b507be1491e1fa2e25
Nyotora <thonio.tiger972@hotmail.com>
2024-03-02 20:30:20 +0100

    feat(csv): add json to csv conversion and csv mail send

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const { Parser } = require('json2csv');
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -12,6 +13,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const amqp = require('amqplib/callback_api');
+const {writeFileSync} = require("fs");
 
 amqp.connect('amqp://localhost', function(error0, connection) {
     if (error0) {
@@ -22,23 +24,70 @@ amqp.connect('amqp://localhost', function(error0, connection) {
             throw error1;
         }
 
-        var queue = 'iut-project';
+        const queue = 'iut-project';
+        const queueCsv = 'iut-project-csv';
 
         channel.assertQueue(queue, {
+            durable: false
+        });channel.assertQueue(queueCsv, {
             durable: false
         });
 
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueCsv);
 
         channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
             const msgContent = JSON.parse(msg.content.toString());
             send(msgContent['to'], msgContent['type'],msgContent['movieJSON'])
         }, {
             noAck: true
         });
+        channel.consume(queueCsv, function(msg) {
+            const msgContent = JSON.parse(msg.content.toString());
+            console.log(msgContent['moviesJSON'])
+            sendCsv(msgContent['to'],msgContent['moviesJSON'])
+        }, {
+            noAck: true
+        });
     });
 });
+
+async function JsonToCSV(json) {
+    const fields = ['id', 'title', 'director', 'releaseDate', 'description'];
+
+    const json2csvParser = new Parser({ fields });
+
+    return json2csvParser.parse(JSON.parse(json));
+}
+
+async function sendCsv(mail,csvJSON) {
+    const filePath = 'movies.csv';
+    const csv = await JsonToCSV(csvJSON)
+    await writeFileSync(filePath, csv, 'utf-8');
+
+    const html = `
+                    <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #ffffff; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+                        <h1 style="color: #333333; text-align: center;">IUT-Project !</h1>
+                        <p style="color: #666666; font-size: 16px; line-height: 1.5;">Retrouvez ci-joint la liste de tous les films enregistrés dans la base de données.</p>
+                    </div>
+            `;
+    const info = await transporter.sendMail({
+        from: '"IUT Project Team" <iut-project@ethereal.email>', // sender address
+        to: mail, // list of receivers
+        subject: "Liste des films", // Subject line
+        text: "Ci*joint les films présents en BDD", // plain text body
+        html: html, // html body
+        attachments: [
+            {
+                filename: 'movies.csv',
+                path: filePath
+            }
+        ]
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
 
 async function send(mails,type,movieJSON = '{}') {
     // send mail with defined transport object
